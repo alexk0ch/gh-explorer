e4a417c13a6941bd5ae100287fa5aed66014ecf6
Alex <as.kochetov@gmail.com>
2015-04-11 00:24:13 +0300

    added some things for repo route

--- Brocfile.js
+++ Brocfile.js
@@ -6,6 +6,7 @@ var app = new EmberApp();
 
 app.import('vendor/bootstrap/dist/js/bootstrap.js');
 app.import('vendor/bootstrap/dist/css/bootstrap.css');
+app.import('vendor/momentjs/moment.js');
 
 var mergeTrees = require('broccoli-merge-trees');
 var pickFiles = require('broccoli-static-compiler');
--- app/user/repositories/route.js
+++ app/user/repositories/route.js
@@ -3,6 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model () {
     var user = this.modelFor('user');
-    return Ember.$.getJSON(user.repos_url);
+    return Ember.$.getJSON(user.repos_url).then((model)=>{
+      return model.map((repo)=>{
+        repo.created_at = moment(repo.created_at, "YYYYMMDD").fromNow();
+        repo.updated_at = moment(repo.updated_at, "YYYYMMDD").fromNow();
+        return repo;  
+      })      
+    });
   }
 });
